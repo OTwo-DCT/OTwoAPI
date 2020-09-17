cb33c8e85d0bbc516df4e1f3f64a42dfe521490e
Suman Mitra <sumanmitra97@gmail.com>
2020-09-17 21:42:59 +0530

    environment setup

--- otwo.ts
+++ otwo.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { config } from 'dotenv';
+
 const app = express();
+config();
 
 app.get("/", (req, res) => {
     res.send('hello...').status(200);
 });
 
-const port = 3500
+const port = process.env.PORT;
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
