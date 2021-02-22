776e251872c2c6df540eb5204d663898c5db71eb
Suman Mitra <sumanmitra97@gmail.com>
2021-02-22 08:37:24 +0000

    bug fixes for dev env setup

--- src/otwo.ts
+++ src/otwo.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { config } from 'dotenv';
+import {config} from 'dotenv';
 
 const app = express();
 config();
 
-app.get("/", (req, res) => {
-    res.send('hello all...').status(200);
+app.get('/', (req, res) => {
+  res.send('hello all...').status(200);
 });
 
 const port = process.env.PORT;
