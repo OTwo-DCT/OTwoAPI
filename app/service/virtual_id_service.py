e9c0b4d478126b498d6e4a95476210720b5ffef0
Suman Mitra <sumanmitra97@gmail.com>
2021-07-06 21:44:56 +0530

    adde heroku files

--- app/service/virtual_id_service.py
+++ app/service/virtual_id_service.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 from flask import Response
-from ..helpers import random
-from main import config
+from ..helpers import random, config
 from ..entity import user
 import traceback
 
 
 def generateVirtualID(phone_number: str) -> Response:
+    CONFIG = config.Config()
     virtual_id = []
     try:
-        for _ in range(config.virtualIDHexArrayLength):
+        for _ in range(CONFIG.virtualIDHexArrayLength):
             while True:
                 hex_str = random.createRandomHexString(
-                    config.virtualIDHexStringLength)
+                    CONFIG.virtualIDHexStringLength)
                 if user.User.objects(virtual_id__contains=hex_str).count() == 0:
                     break
             virtual_id.append(hex_str)
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from app.helpers import config, mongo
 from app.routes import *
 
-config = config.Config()
-mongo.connectMongoDB(f"{config.mongoURI}/{config.mongoDB}?retryWrites=true&w=majority")
+
+CONFIG = config.Config()
+mongo.connectMongoDB(
+    f"{CONFIG.mongoURI}/{CONFIG.mongoDB}?retryWrites=true&w=majority")
 app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(host=config.host, port=config.port,
-            debug=config.isDev, load_dotenv=True)
+    app.run(host=CONFIG.host, port=CONFIG.port,
+            debug=CONFIG.isDev, load_dotenv=True)
